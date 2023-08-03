50c73a4fc58bc53ea78910bead1356bb1c484c54
eyearian <ericyearian@gmail.com>
2023-08-03 15:31:18 -0500

    redoing GET method and lambda permission arn

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -16,49 +16,38 @@ resource "aws_api_gateway_stage" "stage" {
   rest_api_id = aws_api_gateway_rest_api.gateway.id
   deployment_id = aws_api_gateway_deployment.deployment.id
   stage_name = "prod"
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.apigateway.arn
+    format = "$context.requestId"
+  }
 }
 
-#POST Method
-resource "aws_api_gateway_method" "gateway" {
+#GET method
+resource "aws_api_gateway_method" "method" {
   rest_api_id = aws_api_gateway_rest_api.gateway.id
   resource_id = aws_api_gateway_resource.resource.id 
-  http_method = "POST"
+  http_method = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.gateway.id
   resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.gateway.http_method
+  http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = var.invoke_arn
 }
 
-
-#OPTIONS Method
-
-resource "aws_api_gateway_method" "gateway_options" {
-  rest_api_id = aws_api_gateway_rest_api.gateway.id
-  resource_id = aws_api_gateway_resource.resource.id 
-  http_method = "OPTIONS"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "integration_options" {
-  rest_api_id             = aws_api_gateway_rest_api.gateway.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.gateway_options.http_method
-  type                    = "MOCK"
-}
-
-resource "aws_api_gateway_method_response" "response_options" {
+resource "aws_api_gateway_method_response" "response" {
   rest_api_id = aws_api_gateway_rest_api.gateway.id
   resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.gateway_options.http_method
+  http_method = aws_api_gateway_method.method.http_method
   status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
-
 
 #Lambda permissions to invoke gateway
 resource "aws_lambda_permission" "apigw_lambda" {
@@ -67,7 +56,11 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = var.function_name
   principal     = "apigateway.amazonaws.com"
 
-# Confirm this is the right arn and method, check wildcard
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "${aws_api_gateway_rest_api.gateway.execution_arn}/*/*/${var.function_name}"
+  source_arn = "${aws_api_gateway_rest_api.gateway.execution_arn}/*/GET/${var.function_name}"
+}
+
+#Log group
+resource "aws_cloudwatch_log_group" "apigateway" {
+  name = "api_gateway_execution_logs_${aws_api_gateway_rest_api.gateway.id}"
 }
