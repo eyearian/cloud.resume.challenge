d1216bbc143160f521c6aea4b1292e4ed81166d9
eyearian <ericyearian@gmail.com>
2023-08-04 10:38:13 -0500

    options method and tls update

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -49,6 +49,34 @@ resource "aws_api_gateway_method_response" "response" {
   }
 }
 
+#OPTIONS Method for Cors
+resource "aws_api_gateway_method" "gateway_options" {
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
+  resource_id = aws_api_gateway_resource.resource.id 
+  http_method = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration_options" {
+  rest_api_id             = aws_api_gateway_rest_api.gateway.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.gateway_options.http_method
+  type                    = "MOCK"
+  uri                     = var.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "response_options" {
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.gateway_options.http_method
+  status_code = "200"
+    response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+  }
+}
+
 #Lambda permissions to invoke gateway
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
